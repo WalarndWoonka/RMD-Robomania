e5d5fa5695a29990e61d92c077c8909104e23117
willbooker350 <codeninjascna3@gmail.com>
2024-05-11 13:09:54 -0400

    Step 30ish?
    Didnt know what step you were on

---
+++ Assets/Scenes/Crusher1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher1 : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
+    }
+}
